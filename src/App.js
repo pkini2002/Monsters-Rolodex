aa4fa6fe63783ad50f98a239f04ccc1f43443d7d
kiniprathiksha <kiniprathikshadev18@gmail.com>
2023-07-13 23:26:05 +0530

    Day 2 Updates Project Complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
+// 1st constructor() is called and then render() is called and then componentDidMount() is called
+
 import './App.css';
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
 
 class App extends Component {
   constructor(){
       super();
-
+      
+      // State is used to maintain the fields which are common to the component
       this.state={
-        monsters:[]
+        monsters:[],
+        searchField:''
       };
-
-      console.log('constructor');
   }
 
   componentDidMount(){
-    console.log('componentDidMount');
-     // 1st time React renders a component on to the page
+     // 1st time React renders a component on to the page this will be loaded and then render() will be called again
      fetch('https://jsonplaceholder.typicode.com/users')
      .then(response=>response.json())
      .then(users=>this.setState({monsters:users}));
   }
 
+  /* Arrow function is used to bind the context of this to the class (Every time the render() is called
+ the arrow function is created again and again so it is not a good practice to use arrow function in
+  render()) */
+
+  onSearchChange=(event)=>{
+     const searchField=event.target.value.toLowerCase();
+     this.setState(()=>{
+        return {searchField}
+     });
+  }
+
   render(){
-    console.log('render');
+      const {monsters,searchField}=this.state;
+      const {onSearchChange}=this;
+
+     // Filter the monsters based on the searchField
+      const filteredMonsters=monsters.filter((monster)=>{
+              return monster.name.toLocaleLowerCase().includes(searchField);
+      });
+
     return(
     <div className="App">
-      <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event)=>{
-        const searchString=event.target.value.toLowerCase();
-         const filteredMonsters=this.state.monsters.filter((monster)=>{
-              return monster.name.toLocaleLowerCase().includes(searchString);
-         });
-
-          this.setState({monsters:filteredMonsters});
-      }}/>
-      <h1>{this.state.monsters.map((monster)=>{
-        return <div key={monster.id}><h1>{monster.name}</h1></div>
-      })}</h1>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox 
+          onChangeHandler={onSearchChange} 
+          placeholder='Search Monsters'
+          className='monsters-search-box'/>
+      <CardList monsters={filteredMonsters}/>
     </div>
     )
   }
